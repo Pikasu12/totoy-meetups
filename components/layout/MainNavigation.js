fad9cde10dff2919187e5fdd1d3a871180d793df
Marco Ramos <marcoramos1218@gmail.com>
2021-09-29 21:48:26 +0800

    Make meetup to crewmates

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -5,14 +5,14 @@ import classes from "./MainNavigation.module.css"
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <div className={classes.logo}>Totoy Crewmates</div>
       <nav>
         <ul>
           <li>
-            <Link href="/">All Meetups</Link>
+            <Link href="/">All Crewmates</Link>
           </li>
           <li>
-            <Link href="/new-meetup">Add New Meetup</Link>
+            <Link href="/new-meetup">Add New Crewmates</Link>
           </li>
         </ul>
       </nav>
